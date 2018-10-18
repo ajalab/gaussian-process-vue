f7703c860337d729ef4cb68cbfdb69238f5ee8e7
Koki Kato <k-kato@nii.ac.jp>
2018-10-18 16:41:47 +0900

    Add controls

--- src/components/GaussianProcess.vue
+++ src/components/GaussianProcess.vue
@@ -1,10 +1,17 @@
 <template>
-    <svg :width="width" :height="height" @click="addPoint">
-        <line x1=0 :y1="viewPort.yorig" :x2="width" :y2="viewPort.yorig" stroke="#333" stroke-width="1px" shape-rendering="crispEdges"/>
-        <line :x1="viewPort.xorig" y1="0" :x2="viewPort.xorig" :y2="height" stroke="#333" stroke-width="1px" shape-rendering="crispEdges"/>
-        <PredictionCurve :x="prediction.x" :y="prediction.y" :e="prediction.e" :viewPort="viewPort" />
-        <circle v-for="p in viewPoints" r="2" :cx="p.x" :cy="p.y" :key="p.id"/>
-    </svg>
+    <div>
+      <svg :width="width" :height="height" @click="addPoint">
+          <line x1=0 :y1="viewPort.yorig" :x2="width" :y2="viewPort.yorig" stroke="#333" stroke-width="1px" shape-rendering="crispEdges"/>
+          <line :x1="viewPort.xorig" y1="0" :x2="viewPort.xorig" :y2="height" stroke="#333" stroke-width="1px" shape-rendering="crispEdges"/>
+          <PredictionCurve :x="prediction.x" :y="prediction.y" :e="prediction.e" :viewPort="viewPort" />
+          <circle v-for="p in viewPoints" r="2" :cx="p.x" :cy="p.y" :key="p.id"/>
+      </svg>
+      <p>
+        <label>h: <input type="number" v-model="h"/></label>
+        <label>β: <input type="number" v-model="beta"/></label>
+        <input type="button" value="clear" @click="points = []"/>
+      </p>
+    </div>
 </template>
 
 <script lang="ts">
@@ -12,8 +19,6 @@ import { Component, Prop, Vue } from "vue-property-decorator";
 import PredictionCurve from "./PredictionCurve.vue";
 import { regression } from "@/gaussian_process";
 import { ViewPort } from "@/util";
-
-const N = 100;
 
 type Prediction = {
   x: Float64Array;
@@ -24,7 +29,7 @@ type Prediction = {
 type Point = {
   x: number;
   y: number;
-  id: number;
+  id?: number;
 };
 
 @Component({
@@ -39,26 +44,30 @@ export default class GaussoianProcess extends Vue {
   private height!: number;
 
   private viewPort: ViewPort = new ViewPort(this.width, this.height, 30, 30);
-  private lastID: number = 0;
   private points: Point[] = [];
+
+  private h: number = 3;
+  private beta: number = 30;
+  private N: number = 100;
 
   private addPoint(event: MouseEvent) {
     const [x, y] = this.viewPort.actual(event.offsetX, event.offsetY);
-    this.points.push({ x, y, id: this.lastID });
-    this.lastID++;
+    this.points.push({ x, y });
   }
 
   get viewPoints(): Point[] {
-    return this.points.map((p: Point) => {
+    return this.points.map((p: Point, id: number) => {
       const [x, y] = this.viewPort.view(p.x, p.y);
-      return { x, y, id: p.id };
+      return { x, y, id };
     });
   }
 
   get prediction(): Prediction {
-    const xt = new Float64Array(N);
-    for (let i = 0; i < N; i++) {
-      xt[i] = (this.width / this.viewPort.xscale) * (i / (N - 1) - 1 / 2);
+    const xt = new Float64Array(this.N);
+    for (let i = 0; i < this.N; i++) {
+      xt[i] =
+        (this.viewPort.width / this.viewPort.xscale) *
+        (i / (this.N - 1) - 1 / 2);
     }
 
     const n = this.points.length;
@@ -68,7 +77,7 @@ export default class GaussoianProcess extends Vue {
       x[i] = this.points[i].x;
       y[i] = this.points[i].y;
     }
-    const [mu, sig] = regression(x, y, xt);
+    const [mu, sig] = regression(x, y, xt, this.h, this.beta);
 
     return {
       x: xt,
@@ -78,3 +87,14 @@ export default class GaussoianProcess extends Vue {
   }
 }
 </script>
+
+<style scoped>
+input[type="number"] {
+  width: 6em;
+  margin-right: 1em;
+}
+
+svg {
+  cursor: crosshair;
+}
+</style>
